2d6a5fb317a1bd5432dabb2a9656c7b41613c50d
haraphat01 <haraphat@gmail.com>
2021-03-27 15:25:51 +0100

    designed the frontend

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  root 'chatroom#index'
+  get 'login', to: 'session#new'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
