48455c2e5db5e619a44d5ff811933dbe8731e6f7
haraphat01 <haraphat@gmail.com>
2021-03-28 09:45:42 +0100

    added the message resource and login page

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_28_061339) do
+ActiveRecord::Schema.define(version: 2021_03_28_064904) do
 
   create_table "messages", force: :cascade do |t|
     t.text "body"
-    t.integer "user_id"
+    t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -26,4 +27,5 @@ ActiveRecord::Schema.define(version: 2021_03_28_061339) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "messages", "users"
 end
