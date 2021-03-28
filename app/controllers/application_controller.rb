1be7fcb428f0c151a9931b09ef114a538eb3f13c
haraphat01 <haraphat@gmail.com>
2021-03-28 13:55:06 +0100

    added the helper method to the page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user, :logged_in?
+    def current_user
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+    def logged_in?
+        !!current_user
+    end
+    def require_user
+        if !logged_in?
+            flash[:error] = "You must logged in to perfrom this action"
+            redirect_to login_path
+        end
+    end
 end
