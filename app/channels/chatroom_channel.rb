8f4cbc0879a2e1abf29abc04cd69929e0e61f475
haraphat01 <haraphat@gmail.com>
2021-04-01 12:01:02 +0100

    Added websocket to from the action cable

---
+++ app/channels/chatroom_channel.rb
@@ -0,0 +1,10 @@
+class ChatroomChannel < ApplicationCable::Channel
+  def subscribed
+     stream_from "chatroom_channel"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+
+end
---
+++ app/javascript/channels/chatroom_channel.js
@@ -0,0 +1,23 @@
+import consumer from "./consumer"
+
+consumer.subscriptions.create("ChatroomChannel", {
+  connected() {
+    // Called when the subscription is ready for use on the server
+  },
+
+  disconnected() {
+    // Called when the subscription has been terminated by the server
+  },
+
+  received(data) {
+    // Called when there's incoming data on the websocket for this channel
+  },
+
+  subscribed: function() {
+    return this.perform('subscribed');
+  },
+
+  unsubscribed: function() {
+    return this.perform('unsubscribed');
+  }
+});
